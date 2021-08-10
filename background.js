5d0a5886463263ebe4f5f6490f75a2aa5539dac7
Gerben Dekker <gerben.dekker@hollandertechniek.nl>
2021-08-10 09:19:36 +0200

    style(all): format src files

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 
 //you cannot use the chrome.windows api in the devtools.js page.
-chrome.windows.onFocusChanged.addListener(function(windowId) {
+chrome.windows.onFocusChanged.addListener(function (windowId) {
 	//send message to devtool.js. Then you can re-evaluate ko.dataFor($0)
-	chrome.tabs.query({active: true}, function(tabs) {
-		if(tabs && tabs.length > 0) {
-			var tab =  tabs[0];
-			if(tab.id >= 0) {
+	chrome.tabs.query({ active: true }, function (tabs) {
+		if (tabs && tabs.length > 0) {
+			var tab = tabs[0];
+			if (tab.id >= 0) {
 				chrome.tabs.sendMessage(tab.id, {})
 			}
 		}
@@ -13,7 +13,7 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
 });
 
 // notify of page refreshes
-chrome.runtime.onConnect.addListener(function(port) {
+chrome.runtime.onConnect.addListener(function (port) {
 	port.onMessage.addListener(function (msg) {
 		if (msg.action === 'register') {
 			var respond = function (tabId, changeInfo, tab) {
